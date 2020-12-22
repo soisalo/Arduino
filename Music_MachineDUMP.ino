04312a0725e682891cdde0a15458e6df6b621482
soisalo <70382578+soisalo@users.noreply.github.com>
2020-12-22 18:11:09 +0200

    Add files via upload

---
+++ Music_MachineDUMP.ino
@@ -0,0 +1,17 @@
+
+
+//
+//int redswitch = 1;
+
+//void setup() {
+ //Serial.begin(9600);
+  //pinMode(redswitch, OUTPUT);
+
+
+//void loop() {
+  //int switchState = digitalRead(redswitch);
+    //Serial.println(switchState);
+    //delay(100);
+    
+    
+    
